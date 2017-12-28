b621e20210528102b9f0e2d8037442bb060aafba
Rocciescobar <roccisen.88@gmail.com>
2017-12-28 17:52:04 -0500

    Añade versión móvil

---
+++ js/data.js
@@ -0,0 +1,67 @@
+var data = {
+  'restaurants': [
+    {
+      'name': 'La Cuchara Brava',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Recomendable para ir con niños',
+      'address': 'Jirón Huiracocha 1833, Jesús María 15072',
+      'phone': '(01) 5045285',
+    },
+    {
+      'name': 'La Baguette',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Panadería',
+      'address': 'Jirón Sanchez Cerro 2134, Jesús María 15072',
+      'phone': '(01) 7155310',
+    },
+    {
+      'name': 'La Cabañita: Restaurante y Eventos',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Ambiente relajado',
+      'address': 'Av. Salaverry, Jesús María 15072',
+      'phone': '(01) 4657890',
+    },
+    {
+      'name': 'Restaurante Cevichería Juancito',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Marisquería',
+      'address': 'Jirón Huiracocha 1662, Jesús María 15072',
+      'phone': '(01) 2613414',
+    },
+    {
+      'name': 'El Aguajal',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'comida selvática',
+      'address': 'Jr. Huiracocha 1498 Jesús Maria, Cercado de Lima 15072',
+      'phone': '(01) 4319758',
+    },
+    {
+      'name': 'La Picante',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Ambiente relajado',
+      'address': 'Av Húsares de Junín 651, Jesús María 15072',
+      'phone': '(01) 2373689',
+    },
+    {
+      'name': 'Centolla Restaurante',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Buenos cócteles',
+      'address': 'Av Arnaldo Marquez 1629, Jesús María 15072',
+      'phone': '(01) 6366354',
+    },
+    {
+      'name': 'Aguaymanto',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Buenos cócteles',
+      'address': 'Av Horacio Urteaga 1500, Jesús María 15072',
+      'phone': '(01) 7172302',
+    },
+    {
+      'name': 'Mar & Fuego Restaurante',
+      'photo': 'https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5517.462362722079!2d-77.05169207374364!3d-12.082341057867362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurantes+o+cafeter%C3%ADas+jesus+maria!5e0!3m2!1ses-419!2spe!4v1514423549282',
+      'phrase': 'Buenos cócteles',
+      'address': 'Av Horacio Urteaga 1498, Jesús María 15072',
+      'phone': '(01) 4235619',
+    },
+  ]
+};
